f22b21b552812c425c0ddfdbc17508bd916ee1d7
markno1-github <markno1.github@gmail.com>
2020-02-15 12:32:22 +0100

    Refactoring base mode. Atom plugins.

--- installer.plugin.zsh
+++ installer.plugin.zsh
@@ -1,34 +1,26 @@
-# -*- mode: sh -*-
-#
-# installer - Installer script to make life easier
-# Copyright (c) 2020 MarkNo1-github
-# Licensed under the 2-clause BSD license.
-#
-# This tweak eliminates the need for manually install stuff 
-#
-# Usage:
-#     install_'STUFF'
-#     install_env_'NAME'
-#
-#     install_all
+: '
+------------------------------------------------------------------------------
+         FILE:  network
+  DESCRIPTION:  oh-my-zsh plugin.
+       AUTHOR:  marco treglia markno1.github@gmail.com
+      VERSION:  1.0.0
 
+Usage:
+     install_@Stuff	- replace stuff with the availables programs
+     install_all	- install all the availables programs
+------------------------------------------------------------------------------
+'
 
-function askRoot(){
-
-[ "$UID" -eq 0 ] || { echo "This script must be run as root."; exit 1;}
-
-}
-
-
+: 'Install Atom'
 function install_atom() {
-
-askRoot
-
-sudo add-apt-repository ppa:webupd8team/atom
-
-sudo apt-get update
-
-sudo apt-get install atom
-
+  sudo add-apt-repository ppa:webupd8team/atom
+  sudo apt-get update
+  sudo apt-get install atom
 }
 
+: ' Atom Plugins '
+function install_atom_plugins(){
+  apm install git-log
+  apm install git-diff-details
+  apm install git-plus
+}
